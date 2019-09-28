71726379d9666a718e91bc9d715570ce1983690b
John Lawrence <thirdjal@users.noreply.github.com>
2019-09-27 23:59:20 -0700

    Update Member admin pages to include the family

--- membership/admin.py
+++ membership/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
+from django.utils.translation import gettext_lazy as _
 
 from .forms import AccountCreationForm, AccountChangeForm
 from .models import Account, Parent, Scout, Family
@@ -8,21 +9,26 @@ from .models import Account, Parent, Scout, Family
 
 class ParentAdmin(admin.ModelAdmin):
     model = Parent
-    list_display = ('short_name', 'last_name', 'account', 'status')
-    list_display_links = ['short_name', 'last_name', 'account']
+    list_display = ('short_name', 'last_name', 'email', 'status')
+    list_display_links = ['short_name', 'last_name', 'email']
     list_select_related = ('account',)
 
 
 class ParentInline(admin.StackedInline):
     model = Parent
-    can_delete = False
-    verbose_name_plural = 'parent'
+    extra = 0
 
 
 class ScoutAdmin(admin.ModelAdmin):
     model = Scout
     list_display = ('short_name', 'last_name', 'age', 'status')
     list_display_links = ['short_name', 'last_name']
+    list_filter = ('status', )
+
+
+class ScoutInline(admin.StackedInline):
+    model = Scout
+    extra = 0
 
 
 class AccountAdmin(UserAdmin):
@@ -50,12 +56,12 @@ class AccountAdmin(UserAdmin):
     def get_short_name(self, instance):
         return instance.profile.short_name()
 
-    get_short_name.short_description = 'Name'
+    get_short_name.short_description = _('Name')
 
     def get_last_name(self, instance):
         return instance.profile.last_name
 
-    get_last_name.short_description = 'Last Name'
+    get_last_name.short_description = _('Last Name')
 
     def get_inline_instances(self, request, obj=None):
         if not obj:
@@ -63,8 +69,12 @@ class AccountAdmin(UserAdmin):
         return super(AccountAdmin, self).get_inline_instances(request, obj)
 
 
+class FamilyAdmin(admin.ModelAdmin):
+    inlines = (ParentInline, ScoutInline)
+
+
 admin.site.unregister(Group)
 admin.site.register(Parent, ParentAdmin)
 admin.site.register(Scout, ScoutAdmin)
 admin.site.register(Account, AccountAdmin)
-admin.site.register(Family)
+admin.site.register(Family, FamilyAdmin)
