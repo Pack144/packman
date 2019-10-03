70405b17c6abfc9845570a5ca539157c16206731
John Lawrence <thirdjal@users.noreply.github.com>
2019-10-02 22:53:15 -0700

    cleanup unused imports

--- membership/signals.py
+++ membership/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import Account, Member, Parent, Scout
+from .models import Account, Parent
 
 
 @receiver(post_save, sender=Account)
@@ -9,6 +9,3 @@ def create_or_update_account_profile(sender, instance, created, **kwargs):
     if created:
         Parent.objects.create(account=instance)
     instance.profile.save()
-
-@receiver(post_save, sender=Member)
-def update_parent_status(sender, )
