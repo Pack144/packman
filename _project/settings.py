759752bae2d007189c0201117cf6ae9d73572e58
John Lawrence <john@users.noreply.github.com>
2019-10-09 20:29:28 -0700

    Add Mozilla's csp middleware, leaving the detault settings for now.

--- _project/settings.py
+++ _project/settings.py
@@ -59,7 +59,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_referrer_policy.middleware.ReferrerPolicyMiddleware'
+    'django_referrer_policy.middleware.ReferrerPolicyMiddleware',
+    'csp.middleware.CSPMiddleware',
 ]
 
 ROOT_URLCONF = '_project.urls'
