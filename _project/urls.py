825a5f86fda959c219c35bcc414c8f7551bdbef5
John Lawrence <john@users.noreply.github.com>
2019-10-12 09:53:24 -0700

    Add the ability to upload custom profile pictures

--- _project/urls.py
+++ _project/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView
-
-from membership.forms import AccountCreationForm
 
 urlpatterns = [
     # Django admin
@@ -30,7 +28,7 @@ urlpatterns = [
     # Local Apps
     path('members/', include('membership.urls')),
     path('', include('pages.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
