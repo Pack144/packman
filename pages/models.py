493010f1ca9b3d3cbd28146c45f6aa641f4b2db5
John Lawrence <thirdjal@users.noreply.github.com>
2019-11-03 22:25:22 -0800

    Enable rich text support for page content using django-ckeditor

--- pages/models.py
+++ pages/models.py
@@ -1,21 +1,30 @@
 from django.db import models
 from django.utils import timezone
 
+from ckeditor_uploader.fields import RichTextUploadingField
 
-class StaticPage(models.Model):
-    PAGE_CHOICES = (
-        ('HOME', 'Home Page'),
-        ('ABOUT', 'About Us Page'),
-        ('HISTORY', 'History Page'),
-    )
-    body = models.TextField(blank=True, null=True)
-    page = models.CharField(max_length=8, choices=PAGE_CHOICES)
+
+class Page(models.Model):
+    title = models.CharField(max_length=128)
+    body = RichTextUploadingField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
     published_on = models.DateTimeField(default=timezone.now, blank=True, null=True)
 
     def __str__(self):
-        return '{} ({})'.format(self.get_page_display(), self.published_on)
+        return self.title
 
     class Meta:
         get_latest_by = 'published_on'
+
+
+class StaticPage(Page):
+    PAGE_CHOICES = (
+        ('HOME', 'Home Page'),
+        ('ABOUT', 'About Us Page'),
+        ('HISTORY', 'History Page'),
+    )
+    page = models.CharField(max_length=8, choices=PAGE_CHOICES)
+
+    def __str__(self):
+        return self.get_page_display()
