ac5d385b1d10af5375e411232ceb67d203056f7a
John Lawrence <thirdjal@users.noreply.github.com>
2019-11-06 10:15:13 -0800

    create initial calendaring functionality in the 'events' app

--- events/models.py
+++ events/models.py
@@ -2,8 +2,6 @@ import uuid
 
 from django.db import models
 
-from address_book import Address
-
 
 class Event(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
