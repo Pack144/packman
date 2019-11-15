e436845767f71c42dbc101db6cf3488da61300eb
John Lawrence <thirdjal@users.noreply.github.com>
2019-11-14 22:24:10 -0800

    Link address_book address functionality to the Events app

--- events/models.py
+++ events/models.py
@@ -2,8 +2,6 @@ import uuid
 
 from django.db import models
 
-from address_book.models import Address
-
 
 class Event(models.Model):
     CATEGORY_CHOICES = (
@@ -20,12 +18,11 @@ class Event(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=32)
     event_location = models.CharField(max_length=64, blank=True, null=True)
-    address = models.OneToOneField(Address, on_delete=models.CASCADE, blank=True, null=True)
     start = models.DateTimeField()
     end = models.DateTimeField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     url = models.URLField(blank=True, null=True)
-    category = models.CharField(max_length=16, choices=CATEGORY_CHOICES, default='PACK')
+    category = models.CharField(max_length=16, choices=CATEGORY_CHOICES, default='OTHER')
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
