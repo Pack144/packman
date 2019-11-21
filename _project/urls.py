242b82b90a36c9c19ef911af06e34e25e6f47232
John Lawrence <thirdjal@users.noreply.github.com>
2019-11-21 10:44:01 -0800

    Beautify error pages

--- _project/urls.py
+++ _project/urls.py
@@ -17,6 +17,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views import defaults as default_views
+
 
 urlpatterns = [
     # Django admin
@@ -38,4 +40,20 @@ if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+        path(
+          "400/",
+          default_views.bad_request,
+          kwargs={"exception": Exception("Bad Request!")},
+        ),
+        path(
+          "403/",
+          default_views.permission_denied,
+          kwargs={"exception": Exception("Permission Denied")},
+        ),
+        path(
+          "404/",
+          default_views.page_not_found,
+          kwargs={"exception": Exception("Page not Found")},
+        ),
+        path("500/", default_views.server_error),
+        ] + urlpatterns
