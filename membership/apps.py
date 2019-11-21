a840b287385e5f725283fb61a93da4d90bb4fdf4
John Lawrence <thirdjal@users.noreply.github.com>
2019-11-21 09:03:02 -0800

    Wrap app names in localizable strings

--- membership/apps.py
+++ membership/apps.py
@@ -1,8 +1,10 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class MembershipConfig(AppConfig):
     name = 'membership'
+    verbose_name = _('Membership')
 
     def ready(self):
         import membership.signals
