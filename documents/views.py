edbb02618fc28177eb08cbf2c7bc076fb3bd4683
John Lawrence <thirdjal@users.noreply.github.com>
2019-11-21 17:48:54 -0800

    Add more detail to document list

--- documents/views.py
+++ documents/views.py
@@ -2,8 +2,13 @@ from django.views.generic import CreateView, DetailView, UpdateView, ListView
 
 from membership.mixins import ActiveMemberOrContributorTestMixin
 
-from .models import Document
+from .models import Category, Document
 
 
 class DocumentListView(ActiveMemberOrContributorTestMixin, ListView):
     model = Document
+
+    def get_context_data(self, **kwargs):
+        context = super(DocumentListView, self).get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
