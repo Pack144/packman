59c4bc76981fd7c7f2b42cca8742334bb33f160a
John Lawrence <thirdjal@users.noreply.github.com>
2019-11-21 16:36:58 -0800

    Migrate permisions mixins into their own file for scaling

--- membership/views.py
+++ membership/views.py
@@ -1,50 +1,34 @@
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
 from django.views.generic import CreateView, DetailView, UpdateView, ListView
 
+from .mixins import ActiveMemberTestMixin, ActiveMemberOrContributorTestMixin
 from .models import Member, Parent, Scout
 
 
-class ActiveMemberTestMixin(UserPassesTestMixin):
-    """ Parents with active cubs should be allowed to view this page """
-    def test_func(self):
-        if self.request.user.is_authenticated:
-            return self.request.user.profile.is_active
-
-
-class ContributorTestMixin(UserPassesTestMixin):
-    """ Contributors should be allowed to view this page """
-    def test_func(self):
-        if self.request.user.is_authenticated:
-            return self.request.user.profile.role == 'C'
-
-
-class MemberListView(ActiveMemberTestMixin, ContributorTestMixin, ListView):
+class MemberListView(ActiveMemberOrContributorTestMixin, ListView):
     model = Member
     paginate_by = 25
     template_name = 'membership/member_list.html'
 
-    @staticmethod
     def get_queryset(self):
         return Member.objects.filter(Q(parent__children__status__contains='A') | Q(scout__status__exact='A'))
 
 
-class ParentListView(ActiveMemberTestMixin, ContributorTestMixin, ListView):
+class ParentListView(ActiveMemberOrContributorTestMixin, ListView):
     model = Parent
     paginate_by = 25
     template_name = 'membership/parent_list.html'
 
-    @staticmethod
-    def get_queryset():
+    def get_queryset(self):
         return Parent.objects.filter(children__status__contains='A')
 
 
-class ScoutListView(ActiveMemberTestMixin, ContributorTestMixin, ListView):
+class ScoutListView(ActiveMemberOrContributorTestMixin, ListView):
     model = Scout
     paginate_by = 25
     template_name = 'membership/scout_list.html'
 
-    @staticmethod
     def get_queryset(self):
         return Scout.objects.filter(status__exact='A')
 
