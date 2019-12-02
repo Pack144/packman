bf248ffb36afcf1c8cec7eda732c6f0ab7260880
John Lawrence <thirdjal@users.noreply.github.com>
2019-12-02 15:12:51 -0800

    Create Dens and associate with scouts

--- dens/urls.py
+++ dens/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 
 from .views import DenDetailView, DensListView
 
-path('dens/', DensListView.as_view(), name='dens_list'),
-path('den/<int:pk>', DenDetailView.as_view(), name='den_detail'),
+urlpatterns = [
+    path('dens/', DensListView.as_view(), name='dens_list'),
+    path('den/<int:pk>', DenDetailView.as_view(), name='den_detail'),
+]
