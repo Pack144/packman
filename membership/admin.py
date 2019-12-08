8a396956a9f2437c3488c355294cb3f97bbeddbb
John Lawrence <thirdjal@users.noreply.github.com>
2019-12-07 17:46:07 -0800

    Provide the ability to filter members and dens based on rank in admin

--- membership/admin.py
+++ membership/admin.py
@@ -10,6 +10,47 @@ from .forms import AccountCreationForm, AccountChangeForm
 from .models import Account, Family, Parent, Scout
 
 
+class AnimalRankListFilter(admin.SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = _('ranks')
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'ranks'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('tigers', _('Tigers')),
+            ('wolves', _('Wolves')),
+            ('bears', _('Bears')),
+            ('jr_weebs', _('Jr. Webelos')),
+            ('sr_weebs', _('Sr. Webelos')),
+            ('animals', _('Animal Ranks')),
+            ('webelos', _('Webelos')),
+        )
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        if self.value() == 'tigers':
+            return queryset.filter(den__rank__exact=2)
+        if self.value() == 'wolves':
+            return queryset.filter(den__rank__exact=3)
+        if self.value() == 'bears':
+            return queryset.filter(den__rank__exact=4)
+        if self.value() == 'jr_weebs':
+            return queryset.filter(den__rank__exact=5)
+        if self.value() == 'sr_weebs':
+            return queryset.filter(den__rank__exact=6)
+        if self.value() == 'animals':
+            return queryset.filter(den__rank__lte=4)
+        if self.value() == 'webelos':
+            return queryset.filter(den__rank__gte=5)
+
+
 class AddressInline(admin.StackedInline):
     model = Address
     extra = 0
@@ -28,9 +69,9 @@ class ParentInline(admin.StackedInline):
 
 @admin.register(Scout)
 class ScoutAdmin(admin.ModelAdmin):
-    list_display = ('name', 'last_name', 'den', 'school', 'grade', 'age', 'status', )
+    list_display = ('name', 'last_name', 'den', 'school', 'grade', 'age', 'status', 'family', )
     list_display_links = ['name', 'last_name']
-    list_filter = ('status', 'den', )
+    list_filter = ('status', 'den', AnimalRankListFilter)
     readonly_fields = ('date_added', 'last_updated', )
     search_fields = ('first_name', 'middle_name', 'nickname', 'last_name', 'email', )
 
