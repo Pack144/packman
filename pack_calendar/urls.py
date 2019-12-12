11382796ba1c906a74297ee29122ee26bae2bcdd
John Lawrence <thirdjal@users.noreply.github.com>
2019-12-12 11:41:57 -0800

    Create event detail view and link to events and home pages

--- pack_calendar/urls.py
+++ pack_calendar/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import EventListView
+from .views import EventListView, EventDetailView
 
 urlpatterns = [
     path('', EventListView.as_view(), name='event_list'),
+    path('<uuid:pk>/', EventDetailView.as_view(), name='event_detail')
 ]
