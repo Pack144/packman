b8092de85a788d056ece4ad84a893373a6630dad
John Lawrence <thirdjal@users.noreply.github.com>
2019-12-21 16:10:43 -0800

    Reduce code duplication by just importing the AnimalRankListFilter from dens.admin into membership.admin

--- membership/admin.py
+++ membership/admin.py
@@ -2,55 +2,15 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import Group
-from django.forms import models as django_forms
 from django.utils.translation import gettext_lazy as _
 
 from easy_thumbnails.fields import ThumbnailerImageField
 from easy_thumbnails.widgets import ImageClearableFileInput
 
 from address_book.models import Address, PhoneNumber
+from dens.admin import AnimalRankListFilter
 
 from .import forms, models
-
-
-class AnimalRankListFilter(admin.SimpleListFilter):
-    # Human-readable title which will be displayed in the right admin sidebar just above the filter options.
-    title = _('ranks')
-
-    # Parameter for the filter that will be used in the URL query.
-    parameter_name = 'ranks'
-
-    def lookups(self, request, model_admin):
-        return (
-            ('tigers', _('Tigers')),
-            ('wolves', _('Wolves')),
-            ('bears', _('Bears')),
-            ('jr_weebs', _('Jr. Webelos')),
-            ('sr_weebs', _('Sr. Webelos')),
-            ('animals', _('Animal Ranks')),
-            ('webelos', _('Webelos')),
-        )
-
-    def queryset(self, request, queryset):
-        """
-        Returns the filtered queryset based on the value
-        provided in the query string and retrievable via
-        `self.value()`.
-        """
-        if self.value() == 'tigers':
-            return queryset.filter(den__rank__exact=2)
-        if self.value() == 'wolves':
-            return queryset.filter(den__rank__exact=3)
-        if self.value() == 'bears':
-            return queryset.filter(den__rank__exact=4)
-        if self.value() == 'jr_weebs':
-            return queryset.filter(den__rank__exact=5)
-        if self.value() == 'sr_weebs':
-            return queryset.filter(den__rank__exact=6)
-        if self.value() == 'animals':
-            return queryset.filter(den__rank__lte=4)
-        if self.value() == 'webelos':
-            return queryset.filter(den__rank__gte=5)
 
 
 class FamilyListFilter(admin.SimpleListFilter):
