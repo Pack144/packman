f5792b0145e768246e04f8f82f7ddadce98268dd
John Lawrence <thirdjal@users.noreply.github.com>
2020-01-16 12:38:40 -0800

    Add iCal feed and button on pack calendar page

--- pack_calendar/urls.py
+++ pack_calendar/urls.py
@@ -5,6 +5,6 @@ from .feeds import EventFeed
 
 urlpatterns = [
     path('', EventListView.as_view(), name='event_list'),
-    path('feed/', EventFeed(), name='event_feed'),
+    path('feed/pack_calendar.ics', EventFeed(), name='event_feed'),
     path('<uuid:pk>/', EventDetailView.as_view(), name='event_detail')
 ]
