8b86aca5eaede42930c6c90f8085644aa80ef4c9
John Lawrence <thirdjal@users.noreply.github.com>
2020-01-17 09:29:37 -0800

    Fix strings

--- dens/models.py
+++ dens/models.py
@@ -17,17 +17,17 @@ class Rank(models.Model):
     BEAR = 4
     JR_WEBE = 5
     SR_WEBE = 6
-    WEEB = 7
+    WEBE = 7
     ARROW = 8
     RANK_CHOICES = (
-        (BOBCAT, _('Bobcat')),
-        (TIGER, _('Tiger')),
-        (WOLF, _('Wolf')),
-        (BEAR, _('Bear')),
-        (JR_WEBE, _('Jr. Webelos')),
-        (SR_WEBE, _('Sr. Webelos')),
-        (WEEB, _('Webelos')),
-        (ARROW, _('Arrow of Light')),
+        (BOBCAT, _("Bobcat")),
+        (TIGER, _("Tiger")),
+        (WOLF, _("Wolf")),
+        (BEAR, _("Bear")),
+        (JR_WEBE, _("Jr. Webelo")),
+        (SR_WEBE, _("Sr. Webelo")),
+        (WEBE, _("Webelo")),
+        (ARROW, _("Arrow of Light")),
     )
     rank = models.PositiveSmallIntegerField(choices=RANK_CHOICES, unique=True)
     description = models.CharField(max_length=128, blank=True, null=True)
@@ -42,18 +42,18 @@ class Rank(models.Model):
 
     class Meta:
         ordering = ['rank']
-        verbose_name = _('Rank')
-        verbose_name_plural = _('Ranks')
+        verbose_name = _("Rank")
+        verbose_name_plural = _("Ranks")
 
     @property
     def category(self):
-        if self.rank < 1:
+        if self.rank < Rank.BOBCAT:
             # We shouldn't see this
             return None
-        elif self.rank <= 4:
+        elif self.rank <= Rank.BEAR:
             # Bobcat - Bear
-            return _('Animal')
-        elif self.rank <= 7:
+            return _("Animal")
+        elif self.rank >= Rank.JR_WEBE:
             # Jr. & Sr. Webelos
             return _('Webelos')
         else:
@@ -63,20 +63,21 @@ class Rank(models.Model):
 class Den(models.Model):
     """ Each cub should be a member of 1 den """
 
-    number = models.PositiveSmallIntegerField(primary_key=True)
+    number = models.PositiveSmallIntegerField(primary_key=True, help_text=_("The Den's Number"))
     rank = models.ForeignKey(Rank, on_delete=models.CASCADE, related_name='dens', blank=True, null=True)
-    patch = models.ImageField(upload_to='dens', blank=True, null=True)
+    patch = models.ImageField(upload_to='dens', blank=True, null=True, help_text=_(
+        "Display an image of the den's patch on the den detail page and member detail pages."))
 
     date_added = models.DateField(default=timezone.now)
     last_updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['number']
-        verbose_name = _('Den')
-        verbose_name_plural = _('Dens')
+        verbose_name = _("Den")
+        verbose_name_plural = _("Dens")
 
     def __str__(self):
-        return 'Den {}'.format(self.number)
+        return f"Den {self.number}"
 
     def get_absolute_url(self):
         return reverse('den_detail', args=[int(self.number)])
