9781eebfd8f40a619d65712766c71f36aadf0be9
John Lawrence <thirdjal@users.noreply.github.com>
2020-01-17 22:11:10 -0800

    Bugfix: Public content on a dynamic page published to the navbar could break the site.

--- pages/context_processors.py
+++ pages/context_processors.py
@@ -7,7 +7,7 @@ def navbar_items(request):
     pages = {'navbar_public_navs': [], 'navbar_private_navs': []}
     for page in DynamicPage.objects.filter(include_in_nav=True):
         if page.content_set.filter(Q(visibility=Content.PUBLIC) | Q(visibility=Content.ANONYMOUS)):
-            page['navbar_public_navs'].append(page)
+            pages['navbar_public_navs'].append(page)
         else:
             pages['navbar_private_navs'].append(page)
 
