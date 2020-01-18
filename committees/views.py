27f010b618419c393fa39a6724078aa136ed0369
John Lawrence <thirdjal@users.noreply.github.com>
2020-01-18 10:34:23 -0800

    Formatting for Akela and Assistant Akela detail pages.

--- committees/views.py
+++ committees/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 from django.views import generic
 
 from membership.mixins import ActiveMemberOrContributorTest
