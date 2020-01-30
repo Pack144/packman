11a3ca68873a2852ec38f3f4f55e43f57c545292
John Lawrence <thirdjal@users.noreply.github.com>
2020-01-30 15:22:51 -0800

    Initial tests on static pages

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,76 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
 
-# Create your tests here.
+
+from .views import AboutPageView, DynamicPage, HomePageView, HistoryPageView, SignUpPageView
+
+
+class AboutPageTests(TestCase):
+
+    def setUp(self):
+        url = reverse('about_page')
+        self.response = self.client.get(url)
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, 'pages/about_page.html')
+
+    def test_aboutpage_url_resolves_aboutpageview(self):
+        view = resolve('/about/')
+        self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)
+
+
+class HomePageTests(TestCase):
+
+    def setUp(self):
+        url = reverse('home_page')
+        self.response = self.client.get(url)
+
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, 'pages/home_page.html')
+
+    def test_homepage_url_resolves_homepageview(self):  # new
+        view = resolve('/')
+        self.assertEqual(
+            view.func.__name__,
+            HomePageView.as_view().__name__
+        )
+
+
+class HistoryPageTests(TestCase):
+
+    def setUp(self):
+        url = reverse('history_page')
+        self.response = self.client.get(url)
+
+    def test_historypage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_historypage_template(self):
+        self.assertTemplateUsed(self.response, 'pages/history_page.html')
+
+    def test_historypage_url_resolves_historypageview(self):
+        view = resolve('/history/')
+        self.assertEqual(view.func.__name__, HistoryPageView.as_view().__name__)
+
+
+class SignUpPageTests(TestCase):
+
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    def test_signuppage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_signuppage_template(self):
+        self.assertTemplateUsed(self.response, 'pages/signup_page.html')
+
+    def test_signuppage_url_resolves_signuppageview(self):
+        view = resolve('/signup/')
+        self.assertEqual(view.func.__name__, SignUpPageView.as_view().__name__)
