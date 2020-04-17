6abf4cb6776eee09811046fb4cab369703a9f882
John Lawrence <thirdjal@users.noreply.github.com>
2020-04-17 04:47:32 -0700

    Fix dynamic-formsets to allow adding and deleting addresses and phone numbers on member profiles

--- address_book/forms.py
+++ address_book/forms.py
@@ -1,3 +1,5 @@
+import re
+
 from django import forms
 
 from crispy_forms.helper import FormHelper
@@ -16,17 +18,28 @@ class AddressFormHelper(FormHelper):
         self.form_tag = False
         self.help_text_inline = True
         self.layout = Layout(
-            Field('type', css_class='col-4'),
-            'street',
-            'street2',
             Row(
-                Column('city', css_class='col-md-6'),
-                Column('state', css_class='col-md-3'),
-                Column('zip_code', css_class='col-md-3')
-            ),
-            'published',
-            Field('uuid', type='hidden'),
-            Field('member', type='hidden'),
+                Column(
+                    Field('street'),
+                    Field('street2'),
+                    Row(
+                        Column('city', css_class='col-md-6'),
+                        Column('state', css_class='col-md-3'),
+                        Column('zip_code', css_class='col-md-3')
+                    ),
+                    Row(
+                        Field('type'),
+                        Field('published'),
+                    ),
+                    Field('uuid', type='hidden'),
+                    Field('member', type='hidden'),
+                ),
+                Column(
+                    Field('DELETE'),
+                    css_class='col-1'
+                ),
+                css_class="address-dynamic-form mb-4"
+            )
         )
         self.render_required_fields = True
 
@@ -38,9 +51,9 @@ class AddressForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(AddressForm, self).__init__(*args, **kwargs)
-        for name, field in self.fields.items():
-            if field.widget.__class__ == forms.widgets.TextInput:
-                field.widget.attrs['placeholder'] = field.label
+        for visible in self.visible_fields():
+            if visible.field.widget.__class__ == forms.widgets.TextInput:
+                visible.field.widget.attrs['placeholder'] = visible.label
         self.helper = AddressFormHelper(self)
         self.fields['state'].choices = STATE_CHOICES + ((None, self.fields['state'].label),)
 
@@ -49,20 +62,31 @@ class PhoneNumberFormHelper(FormHelper):
 
     def __init__(self, *args, **kwargs):
         super(PhoneNumberFormHelper, self).__init__(*args, **kwargs)
+
+        formtag_prefix = re.sub('-[0-9]+$', '', kwargs.get('prefix', ''))
+
         self.disable_csrf = True
         self.form_show_labels = False
         self.form_tag = False
         self.help_text_inline = True
         self.layout = Layout(
             Row(
-                Column('number', css_class='col-md-6'),
-                Column('type', css_class='col-md-2'),
+                Column(
+                    Field('number'),
+                    Row(
+                        Field('type'),
+                        Field('published'),
+                    ),
+                    Field('uuid', type='hidden'),
+                    Field('member', type='hidden'),
+                ),
+                Column(
+                    Field('DELETE'),
+                    css_class='col-1'
+                ),
+                css_class="phonenumber-dynamic-form mb-4"
             ),
-            Field('published'),
-            Field('uuid', type='hidden'),
-            Field('member', type='hidden'),
         )
-        self.render_required_fields = True
 
 
 class PhoneNumberForm(forms.ModelForm):
@@ -72,7 +96,7 @@ class PhoneNumberForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(PhoneNumberForm, self).__init__(*args, **kwargs)
-        for name, field in self.fields.items():
-            if field.widget.__class__ == forms.widgets.TextInput:
-                field.widget.attrs['placeholder'] = field.label
+        for visible in self.visible_fields():
+            if visible.field.widget.__class__ == forms.widgets.TextInput:
+                visible.field.widget.attrs['placeholder'] = visible.label
         self.helper = PhoneNumberFormHelper(self)
