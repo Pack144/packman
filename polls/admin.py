4a6d6976fb94ced45b8248f6a6b316312c33e1e9
John Lawrence <thirdjal@users.noreply.github.com>
2020-04-16 20:42:20 -0700

    Families get one vote per active cub.

--- polls/admin.py
+++ polls/admin.py
@@ -1,22 +1,22 @@
 from django.contrib import admin
-from django.utils import timezone
 
 from .models import Choice, Question
 
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
-    extra = 3
+    readonly_fields = ['count_votes', ]
+    extra = 0
 
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        (None, {'fields': ['question_text', 'description', 'is_anonymous']}),
+        ('Date information', {'fields': ['poll_opens', 'poll_closes']}),
     ]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
-    list_filter = ['pub_date']
+    list_display = ('question_text', 'count_choices', 'count_total_votes', 'poll_opens', 'poll_closes', 'was_published_recently')
+    list_filter = ['poll_opens', 'poll_closes']
     search_fields = ['question_text']
 
 
