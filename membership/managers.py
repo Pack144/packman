6836beff0e8de90b0988da8abcf8804d381e21d0
John Lawrence <thirdjal@users.noreply.github.com>
2020-04-25 22:21:04 -0700

    Create new filter managers to make it easier to find cubs based on rank

--- membership/managers.py
+++ membership/managers.py
@@ -1,4 +1,8 @@
 from django.contrib.auth.models import UserManager
+from django.db import models
+
+from dens.models import Rank
+from pack_calendar.models import PackYear
 
 
 class MemberManager(UserManager):
@@ -33,3 +37,45 @@ class MemberManager(UserManager):
             raise ValueError('Superuser must have is_superuser=True.')
 
         return self._create_user(email, password, **extra_fields)
+
+
+class CurrentTigersManager(models.Manager):
+    def get_query_set(self):
+        return self.objects.filter(den__year_assigned=PackYear.get_current_pack_year()).filter(
+            den__den__rank__rank=Rank.TIGER)
+
+
+class CurrentWolvesManager(models.Manager):
+    def get_query_set(self):
+        return self.objects.filter(den__year_assigned=PackYear.get_current_pack_year()).filter(
+            den__den__rank__rank=Rank.WOLF)
+
+
+class CurrentBearsManager(models.Manager):
+    def get_query_set(self):
+        return self.objects.filter(den__year_assigned=PackYear.get_current_pack_year()).filter(
+            den__den__rank__rank=Rank.BEAR)
+
+
+class CurrentJrWebesManager(models.Manager):
+    def get_query_set(self):
+        return self.objects.filter(den__year_assigned=PackYear.get_current_pack_year()).filter(
+            den__den__rank__rank=Rank.JR_WEBE)
+
+
+class CurrentSrWebesManager(models.Manager):
+    def get_query_set(self):
+        return self.objects.filter(den__year_assigned=PackYear.get_current_pack_year()).filter(
+            den__den__rank__rank=Rank.SR_WEBE)
+
+
+class CurrentAnimalsManager(models.Manager):
+    def get_query_set(self):
+        return self.objects.filter(den__year_assigned=PackYear.get_current_pack_year()).filter(
+            den__den__rank__rank__lte=Rank.BEAR)
+
+
+class CurrentWebelosManager(models.Manager):
+    def get_query_set(self):
+        return self.objects.filter(den__year_assigned=PackYear.get_current_pack_year()).filter(
+            den__den__rank__rank__gte=Rank.JR_WEBE)
