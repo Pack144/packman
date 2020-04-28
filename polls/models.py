fa35fad6dca21e0a205cd39e8deef523b3c7601a
John Lawrence <thirdjal@users.noreply.github.com>
2020-04-28 13:58:43 -0700

    Apply codestyle fixes from flake8

--- polls/models.py
+++ polls/models.py
@@ -11,13 +11,35 @@ def two_weeks_hence(when=timezone.now()):
 
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    description = RichTextField(blank=True, null=True)
-    poll_opens = models.DateTimeField(_("poll opens"), default=timezone.now, blank=True)
-    poll_closes = models.DateTimeField(_("poll closes"), default=two_weeks_hence, blank=True)
+    question_text = models.CharField(
+        max_length=200,
+    )
+    description = RichTextField(
+        blank=True,
+        null=True,
+    )
+    poll_opens = models.DateTimeField(
+        _("poll opens"),
+        default=timezone.now,
+        blank=True,
+    )
+    poll_closes = models.DateTimeField(
+        _("poll closes"),
+        default=two_weeks_hence,
+        blank=True,
+    )
 
-    is_anonymous = models.BooleanField(_("anonymous"), default=False)
-    is_editable = models.BooleanField(_("editable"), default=True)
+    is_anonymous = models.BooleanField(
+        _("anonymous"),
+        default=False,
+    )
+    is_editable = models.BooleanField(
+        _("editable"),
+        default=True,
+    )
+
+    class Meta:
+        ordering = ('-poll_closes', )
 
     def __str__(self):
         return self.question_text
@@ -25,12 +47,11 @@ class Question(models.Model):
     def get_absolute_url(self):
         return reverse_lazy('polls:detail', kwargs={'pk': self.pk})
 
-    class Meta:
-        ordering = ('-poll_closes', )
-
     def can_vote(self, member):
         if self.poll_opens <= timezone.now() <= self.poll_closes:
-            return member.get_active_scouts().count() - self.vote_set.filter(family=member.family).count()
+            return member.get_active_scouts().count() - self.vote_set.filter(
+                family=member.family
+            ).count()
 
     def count_choices(self):
         return self.choice_set.count()
@@ -51,8 +72,13 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE,
+    )
+    choice_text = models.CharField(
+        max_length=200,
+    )
 
     def __str__(self):
         return self.choice_text
@@ -62,11 +88,25 @@ class Choice(models.Model):
 
 
 class Vote(models.Model):
-    family = models.ForeignKey('membership.Family', on_delete=models.CASCADE)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    family = models.ForeignKey(
+        'membership.Family',
+        on_delete=models.CASCADE,
+    )
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE,
+    )
+    choice = models.ForeignKey(
+        Choice,
+        on_delete=models.CASCADE,
+    )
 
-    last_updated = models.DateTimeField(auto_now=True)
+    date_added = models.DateTimeField(
+        auto_now_add=True,
+    )
+    last_updated = models.DateTimeField(
+        auto_now=True,
+    )
 
     def __str__(self):
         return f"Vote for {self.choice}"
