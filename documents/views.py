9b9f36cb2bf4ca9293fe3fe034b56a889b7c0c13
John Lawrence <thirdjal@users.noreply.github.com>
2020-04-27 23:48:23 -0700

    Apply codestyle fixes from flake8

--- documents/views.py
+++ documents/views.py
@@ -7,11 +7,20 @@ from .models import Category, Document
 
 
 class DocumentListView(ActiveMemberOrContributorTest, ListView):
-    """ Display a listing of all the documents published to the repository """
+    """
+    Display a listing of all the documents published to the repository
+    """
     model = Document
     template_name = 'documents/document_list.html'
 
     def get_queryset(self):
-        """ Provide Category data and filter based on whether document should be displayed """
-        return Category.objects.prefetch_related(Prefetch('documents',
-                                                          queryset=Document.objects.filter(display_in_repository=True)))
+        """
+        Provide Category data and filter based on whether document should be
+        displayed
+        """
+        return Category.objects.prefetch_related(
+            Prefetch(
+                'documents',
+                queryset=Document.objects.filter(display_in_repository=True)
+            )
+        )
