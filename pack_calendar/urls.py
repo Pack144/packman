2039d90a7ea9a152a27df634032469a8fa7b8c60
John Lawrence <thirdjal@users.noreply.github.com>
2020-04-28 14:18:43 -0700

    Apply codestyle fixes from flake8

--- pack_calendar/urls.py
+++ pack_calendar/urls.py
@@ -4,7 +4,19 @@ from .views import EventListView, EventDetailView
 from .feeds import EventFeed
 
 urlpatterns = [
-    path('', EventListView.as_view(), name='event_list'),
-    path('<uuid:pk>/', EventDetailView.as_view(), name='event_detail'),
-    path(f'feed/<uuid:family>/{EventFeed.file_name}', EventFeed(), name='event_feed'),
+    path(
+        '',
+        EventListView.as_view(),
+        name='event_list'
+    ),
+    path(
+        '<uuid:pk>/',
+        EventDetailView.as_view(),
+        name='event_detail'
+    ),
+    path(
+        f'feed/<uuid:family>/{EventFeed.file_name}',
+        EventFeed(),
+        name='event_feed'
+    ),
 ]
