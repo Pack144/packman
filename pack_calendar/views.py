993f6042a635f2fa3b929e9367990771473f281e
John Lawrence <thirdjal@users.noreply.github.com>
2020-04-28 14:18:43 -0700

    Apply codestyle fixes from flake8

--- pack_calendar/views.py
+++ pack_calendar/views.py
@@ -18,8 +18,11 @@ class EventListView(ActiveMemberOrContributorTest, ListView):
         """
         Return a queryset containing all events for the next 6 months
         """
-        return Event.objects.filter(start__lte=timezone.now() + timezone.timedelta(weeks=26)).filter(
-            start__gte=timezone.now() - timezone.timedelta(hours=8)).order_by('start')
+        return Event.objects.filter(
+            start__lte=timezone.now() + timezone.timedelta(weeks=26)
+        ).filter(
+            start__gte=timezone.now() - timezone.timedelta(hours=8)
+        ).order_by('start')
 
 
 class EventDetailView(ActiveMemberOrContributorTest, DetailView):
