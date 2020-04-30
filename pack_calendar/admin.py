8ec1c052e7eabece634d300dae8de9d4e38ed3c6
John Lawrence <thirdjal@users.noreply.github.com>
2020-04-29 23:28:16 -0700

    Make it easier to change the status of an event in admin

--- pack_calendar/admin.py
+++ pack_calendar/admin.py
@@ -3,6 +3,18 @@ from django.contrib import admin
 from .models import Category, Event, PackYear
 
 
+def mark_events_cancelled(modeladmin, request, queryset):
+    queryset.update(status=Event.CANCELLED)
+
+
+def mark_events_confirmed(modeladmin, request, queryset):
+    queryset.update(status=Event.CONFIRMED)
+
+
+def mark_events_tentative(modeladmin, request, queryset):
+    queryset.update(status=Event.TENTATIVE)
+
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'color', 'icon', )
@@ -10,8 +22,9 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
+    actions = [mark_events_cancelled, mark_events_confirmed, mark_events_tentative]
     autocomplete_fields = ('attendees', 'attendee_groups')
-    list_display = ('name', 'get_location', 'start', 'end', 'category', )
+    list_display = ('name', 'get_location', 'start', 'end', 'category', 'status')
     list_filter = ('category', )
     search_fields = ('name', 'start', 'end', 'location', 'venue__name')
     readonly_fields = ('duration', )
