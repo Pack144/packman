8f4166ed33040bf6916f85ff981a693ce228a66b
John Lawrence <thirdjal@users.noreply.github.com>
2020-05-04 10:46:18 -0700

    Set the verbose name for Post Office

--- post_office/apps.py
+++ post_office/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class PostOfficeConfig(AppConfig):
     name = 'post_office'
+    verbose_name = _("Post Office")
