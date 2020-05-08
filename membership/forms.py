41f297c254bd42bdedb28e739ecec4604bc8f09b
John Lawrence <thirdjal@users.noreply.github.com>
2020-05-08 16:17:15 -0700

    When a new cub application has been received, send a basic email

--- membership/forms.py
+++ membership/forms.py
@@ -1,19 +1,20 @@
 from django import forms
+from django.conf import settings
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.core.mail import send_mail
 from django.forms import widgets
 from django.forms.models import inlineformset_factory
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
+from crispy_forms.bootstrap import AppendedText, FormActions, InlineRadios
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit, Row, Column, Field
-from crispy_forms.bootstrap import FormActions, InlineRadios, AppendedText
+from crispy_forms.layout import Column, Field, Fieldset, Layout, Row, Submit
 from tempus_dominus.widgets import DatePicker
 
 from address_book.forms import AddressForm, PhoneNumberForm
 from address_book.models import Address, PhoneNumber
-
-from .models import Adult, Scout, Family
+from .models import Adult, Family, Scout
 
 AddressFormSet = inlineformset_factory(
     Adult,
@@ -241,6 +242,66 @@ class ScoutForm(forms.ModelForm):
             ),
         )
 
+    def notify_membership(self, submitter):
+        """
+        TODO: remove hard-coded addresses & message body
+        Send an e-mail to the membership committee notifying them of a new
+        application to review.
+        """
+        subject = (
+            f"[{settings.PACK_SHORTNAME}] New member application - "
+            f"{self.cleaned_data['first_name']} {self.cleaned_data['last_name']}"
+        )
+        message = (
+            f"Hello Membership Committee,\n"
+            f"\n"
+            f"This email confirms that {self.cleaned_data['first_name']} "
+            f"{self.cleaned_data['last_name']}, has applied for membership in "
+            f"{settings.PACK_NAME}. Please login to pack144.org at your "
+            f"earliest convenience to review their application."
+        )
+        to_field = ('Membership Committee <membership@pack144.org>',)
+        from_field = 'Cub Scouts Pack 144 <noreply@pack144.org>'
+        send_mail(
+            subject=subject,
+            message=message,
+            from_email=from_field,
+            recipient_list=to_field
+        )
+
+    def send_confirmation_email(self, submitter):
+        """
+        TODO: remove hard-coded addresses & message body
+        Send an e-mail to the cub's parent confirming their application has
+        has been received.
+        """
+        subject = (
+            f"[{settings.PACK_SHORTNAME}] New member application - "
+            f"{self.cleaned_data['first_name']} {self.cleaned_data['last_name']}"
+        )
+
+        message = (
+            f"Hello {submitter},\n"
+            '\n'
+            f"This email confirms the receipt of "
+            f"{self.cleaned_data['first_name']}'s application to join Pack "
+            f"144.\nPlease note that receipt of your application does not "
+            f"guarantee membership to the Pack. You will be\n be notified "
+            f"ASAP of the status of your application.\n"
+            '\n'
+            'If you have any questions in the mean time, please contact us at '
+            'membership@pack144.org.'
+        )
+
+        to_field = (f'{submitter} <{submitter.email}>',)
+        from_field = 'Cub Scouts Pack 144 <noreply@pack144.org>'
+        send_mail(
+            subject=subject,
+            message=message,
+            from_email=from_field,
+            recipient_list=to_field
+        )
+
 
 class SignupForm(UserCreationForm):
     class Meta:
@@ -255,12 +316,12 @@ class SignupForm(UserCreationForm):
             'is_published',
             'gender',
             'role',
-            'photo'
+            'photo',
         )
         widgets = {
             'gender': widgets.RadioSelect,
             'role': widgets.RadioSelect,
-            'photo': widgets.FileInput
+            'photo': widgets.FileInput,
         }
 
     def __init__(self, *args, **kwargs):
@@ -275,44 +336,49 @@ class SignupForm(UserCreationForm):
         self.fields['email'].widget.attrs['autofocus'] = False
         self.helper = FormHelper(self)
         self.helper.form_id = 'parent_update'
+        self.helper.form_tag = False
         self.helper.form_show_labels = False
         self.helper.layout = Layout(
-            Row(
-                Column('first_name', css_class='col-md-4'),
-                Column('middle_name', css_class='col-md-2 text-truncate'),
-                Column('last_name', css_class='col-md-5'),
-                Column('suffix', css_class='col-md-1'),
-            ),
-            Field('nickname', css_class='col-md-4'),
-            Row(
-                Column('email'),
-                Column('is_published'),
-            ),
-            Row(
-                Column('password1', autocomplete='off'),
-                Column('password2', autocomplete='off'),
-            ),
-            Row(
-                Column(InlineRadios('gender')),
-                Column(InlineRadios('role')),
-            ),
-            Field('photo', css_class='col-md-4'),
-            FormActions(
-                Submit('save', 'Sign me up!', css_class='btn-success btn-lg btn-block'),
+            Fieldset(
+                'Your Details',
+                Row(
+                    Column('first_name', css_class='col-md-4'),
+                    Column('middle_name', css_class='col-md-2 text-truncate'),
+                    Column('last_name', css_class='col-md-5'),
+                    Column('suffix', css_class='col-md-1'),
+                ),
+                Row(
+                    Column('nickname', css_class='col-md-4'),
+                ),
+                Row(
+                    Column('email'),
+                    Column('is_published'),
+                ),
+                Row(
+                    Column('password1', autocomplete='off'),
+                    Column('password2', autocomplete='off'),
+                ),
+                Row(
+                    Column(InlineRadios('gender')),
+                    Column(InlineRadios('role')),
+                ),
+                Row(
+                    Column('photo', css_class='col-md-6'),
+                ),
             ),
         )
-
-    def signup(self, request, user):
-        user.first_name = self.cleaned_data['first_name']
-        user.middle_name = self.cleaned_data['middle_name']
-        user.last_name = self.cleaned_data['last_name']
-        user.suffix = self.cleaned_data['suffix']
-        user.nickname = self.cleaned_data['nickname']
-        user.gender = self.cleaned_data['gender']
-        user.role = self.cleaned_data['role']
-        user.photo = self.cleaned_data['photo']
-        user.email = self.cleaned_data['email']
-        user.is_published = self.cleaned_data['is_published']
-        user.family = FamilyForm.objects.create()
-        user.save()
-        return user
+    #
+    # def signup(self, request, user):
+    #     user.first_name = self.cleaned_data['first_name']
+    #     user.middle_name = self.cleaned_data['middle_name']
+    #     user.last_name = self.cleaned_data['last_name']
+    #     user.suffix = self.cleaned_data['suffix']
+    #     user.nickname = self.cleaned_data['nickname']
+    #     user.gender = self.cleaned_data['gender']
+    #     user.role = self.cleaned_data['role']
+    #     user.photo = self.cleaned_data['photo']
+    #     user.email = self.cleaned_data['email']
+    #     user.is_published = self.cleaned_data['is_published']
+    #     user.family = Family.objects.create()
+    #     user.save()
+    #     return user
