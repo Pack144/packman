3e8afb4b5f33ff32c4e59429671423d09e123400
John Lawrence <thirdjal@users.noreply.github.com>
2020-05-29 09:27:56 -0700

    Correct for renamed 'is_staff' field

--- membership/managers.py
+++ membership/managers.py
@@ -24,15 +24,15 @@ class MemberManager(UserManager):
         return user
 
     def create_user(self, email=None, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('_is_staff', False)
         extra_fields.setdefault('is_superuser', False)
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email=None, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('_is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
-        if extra_fields.get('is_staff') is not True:
+        if extra_fields.get('_is_staff') is not True:
             raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
