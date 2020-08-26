1e5e4f8a73ad17fec884762bd306df90acfb3a4c
John Lawrence <thirdjal@users.noreply.github.com>
2020-08-26 11:43:03 -0700

    Namespace dens app

--- dens/urls.py
+++ dens/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from .views import DenDetailView, DensListView
 
+app_name = 'dens'
 urlpatterns = [
-    path('', DensListView.as_view(), name='dens_list'),
-    path('<int:pk>/', DenDetailView.as_view(), name='den_detail'),
+    path('', DensListView.as_view(), name='list'),
+    path('<int:pk>/', DenDetailView.as_view(), name='detail'),
 ]
