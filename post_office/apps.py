2fe746520c773f85c3c862113503d72bb6cd78c8
John Lawrence <thirdjal@users.noreply.github.com>
2020-11-03 14:03:36 -0800

    Delete the post_office App (never used)

--- post_office/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
-
-
-class PostOfficeConfig(AppConfig):
-    name = 'post_office'
-    verbose_name = _("Post Office")
