f4a6e5ef343f7a5ac648180f44223055f215e86f
John Lawrence <thirdjal@users.noreply.github.com>
2020-11-03 13:10:29 -0800

    Simplify Page models by merging Static & Dynamic into the parent model.

--- pages/admin.py
+++ pages/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
 
-from .models import Category, ContentBlock, DynamicPage, StaticPage
-
-
-class CategoryInline(admin.TabularInline):
-    model = DynamicPage.categories.through
-    extra = 0
+from .models import ContentBlock, Page
 
 
 class ContentBlockInline(admin.StackedInline):
@@ -13,29 +8,10 @@ class ContentBlockInline(admin.StackedInline):
     extra = 0
 
 
-@admin.register(ContentBlock)
-class ContentBlockAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'page')
-    list_display_links = ('__str__', )
-    list_filter = ('page',)
-    search_fields = ['title', 'body']
-
-
-@admin.register(DynamicPage)
-class DynamicPageAdmin(admin.ModelAdmin):
-    exclude = ['categories', ]
-    inlines = [CategoryInline, ContentBlockInline]
-    list_display = ('title', 'last_updated', )
-    list_filter = ('categories', )
-    prepopulated_fields = {'slug': ('title', )}
-    search_fields = ['title', 'categories', 'published_on', ]
-
-
-@admin.register(StaticPage)
-class StaticPageAdmin(admin.ModelAdmin):
-    list_display = ('page', 'title', 'last_updated')
-    list_display_links = ('page', 'title')
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
     inlines = [ContentBlockInline]
-
-
-admin.site.register(Category)
+    list_display = ('title', 'page', 'last_updated')
+    list_display_links = ('title', 'page')
+    prepopulated_fields = {'slug': ('title', )}
+    search_fields = ['title', 'published_on', ]
