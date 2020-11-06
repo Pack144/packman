62cb6d459751fee2e23a17a35a93ba57e859f314
John Lawrence <thirdjal@users.noreply.github.com>
2020-11-05 22:18:57 -0800

    rename visible function in ContentBlockManager

--- pages/context_processors.py
+++ pages/context_processors.py
@@ -4,6 +4,6 @@ from .models import Page
 def populate_navbar(request):
     navbar = {'navbar_links': []}
     for page in Page.objects.filter(include_in_nav=True):
-        if page.content_blocks.visible(user=request.user):
+        if page.content_blocks.get_visible(user=request.user):
             navbar['navbar_links'].append(page)
     return navbar
--- pages/managers.py
+++ pages/managers.py
@@ -3,31 +3,23 @@ from django.db import models
 
 class ContentBlockQuerySet(models.QuerySet):
     def anonymous(self):
-        return self.filter(
-            visibility__exact=self.model.ANONYMOUS,
-        )
+        return self.filter(visibility__exact=self.model.ANONYMOUS)
 
     def authenticated(self):
-        return self.filter(
-            visibility__exact=self.model.PUBLIC,
-        )
+        return self.filter(visibility__exact=self.model.PUBLIC)
 
     def public(self):
-        return self.filter(
-            visibility__lte=self.model.PUBLIC,
-        )
+        return self.filter(visibility__lte=self.model.PUBLIC)
 
     def private(self):
-        return self.filter(
-            visibility__gte=self.model.PUBLIC,
-        )
+        return self.filter(visibility__gte=self.model.PUBLIC)
 
 
 class ContentBlockManager(models.Manager):
     def get_queryset(self):
         return ContentBlockQuerySet(self.model, using=self._db)
 
-    def visible(self, user):
+    def get_visible(self, user):
         if user.is_anonymous:
             return self.get_queryset().public()
         elif user.active():
