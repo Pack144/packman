0e1682b1a3fd2c388a2a0516665d41a6b281102d
John Lawrence <thirdjal@users.noreply.github.com>
2021-01-13 09:43:18 -0800

    Prettify Member List in admin

--- address_book/admin.py
+++ address_book/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.html import format_html, format_html_join
+from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
 
 from .models import Address, PhoneNumber, Venue, Category, DistributionList
@@ -53,7 +55,8 @@ class DistributionListAdmin(admin.ModelAdmin):
     get_committee_list.short_description = _("committees")
 
     def get_member_list(self, obj):
-        return "\n".join(f"{n} <{a}>" for n, a in obj.email_addresses)
+        members = format_html_join("", "<li>{}</li>", ((mark_safe(f"<strong>{n}</strong> <em>&lt;{a}&gt;</em>"), ) for n, a in obj.email_addresses))
+        return format_html("<ul>{}</ul>", members) if members else ""
 
     get_member_list.short_description = _("members")
 
