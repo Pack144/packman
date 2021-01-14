1209ec670566d81ff9043f488b1ddc8128d4a73c
John Lawrence <thirdjal@users.noreply.github.com>
2021-01-14 14:07:23 -0800

    Refactor pages app into packman project folder

--- config/urls.py
+++ config/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
     path('documents/', include('packman.documents.urls')),
     path('calendar/', include('packman.calendars.urls')),
     path('members/', include('membership.urls')),
-    path('', include('pages.urls')),
+    path('', include('packman.pages.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
