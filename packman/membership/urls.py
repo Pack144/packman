3e3792739656c6d008af7075e3be251a965d42e9
John Lawrence <thirdjal@users.noreply.github.com>
2021-04-07 08:56:28 -0700

    Simplify urlpatterns

--- packman/membership/urls.py
+++ packman/membership/urls.py
@@ -1,70 +1,18 @@
 from django.urls import path
 
-from .views import (
-    AdultCreate,
-    AdultDetail,
-    AdultList,
-    AdultUpdate,
-    MemberList,
-    MyFamilyDetail,
-    MemberSearchResultsList,
-    ScoutCreate,
-    ScoutDetail,
-    ScoutList,
-    ScoutUpdate,
-)
+from . import views
 
 app_name = "membership"
 urlpatterns = [
-    path(
-        "",
-        MemberList.as_view(),
-        name="all",
-    ),
-    path(
-        "adults/",
-        AdultList.as_view(),
-        name="parents",
-    ),
-    path(
-        "adult/add/",
-        AdultCreate.as_view(),
-        name="parent_create",
-    ),
-    path(
-        "adults/<slug:slug>/",
-        AdultDetail.as_view(),
-        name="parent_detail",
-    ),
-    path(
-        "adult/<uuid:pk>/update/",
-        AdultUpdate.as_view(),
-        name="parent_update",
-    ),
-    path(
-        "cubs/",
-        ScoutList.as_view(),
-        name="scouts",
-    ),
-    path(
-        "cub/add/",
-        ScoutCreate.as_view(),
-        name="scout_create",
-    ),
-    path(
-        "cubs/<slug:slug>/",
-        ScoutDetail.as_view(),
-        name="scout_detail",
-    ),
-    path(
-        "cub/<uuid:pk>/update/",
-        ScoutUpdate.as_view(),
-        name="scout_update",
-    ),
-    path(
-        "search/",
-        MemberSearchResultsList.as_view(),
-        name="search_results",
-    ),
-    path("my-family/", MyFamilyDetail.as_view(), name="my-family"),
+    path("", views.MemberList.as_view(), name="all"),
+    path("adults/", views.AdultList.as_view(), name="parents"),
+    path("adult/add/", views.AdultCreate.as_view(), name="parent_create"),
+    path("adults/<slug:slug>/", views.AdultDetail.as_view(), name="parent_detail"),
+    path("adult/<uuid:pk>/update/", views.AdultUpdate.as_view(), name="parent_update"),
+    path("cubs/", views.ScoutList.as_view(), name="scouts"),
+    path("cub/add/", views.ScoutCreate.as_view(), name="scout_create"),
+    path("cubs/<slug:slug>/", views.ScoutDetail.as_view(), name="scout_detail"),
+    path("cub/<uuid:pk>/update/", views.ScoutUpdate.as_view(), name="scout_update"),
+    path("search/", views.MemberSearchResultsList.as_view(), name="search_results"),
+    path("my-family/", views.MyFamilyDetail.as_view(), name="my-family"),
 ]
