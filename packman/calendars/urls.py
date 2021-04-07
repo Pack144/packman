7abd5f29a2aa91082f3d9c7198985fb17be791fe
John Lawrence <thirdjal@users.noreply.github.com>
2021-04-07 08:46:24 -0700

    Simplify urlpatterns

--- packman/calendars/urls.py
+++ packman/calendars/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
-from .feeds import EventFeed
-from .views import EventDetailView, EventListView, EventArchiveView
+from . import feeds, views
 
 app_name = "calendars"
 urlpatterns = [
-    path("", EventListView.as_view(), name="list"),
-    path("<uuid:pk>/", EventDetailView.as_view(), name="detail"),
-    path("archive/", EventArchiveView.as_view(), name="archive"),
-    path("feed/<uuid:family_uuid>.ics", EventFeed(), name="feed"),
+    path("", views.EventListView.as_view(), name="list"),
+    path("<uuid:pk>/", views.EventDetailView.as_view(), name="detail"),
+    path("archive/", views.EventArchiveView.as_view(), name="archive"),
+    path("feed/<uuid:family_uuid>.ics", feeds.EventFeed(), name="feed"),
 ]
