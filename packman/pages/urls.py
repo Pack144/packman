5cdb0d11c96748151e3153bf62cc389f139214ae
John Lawrence <thirdjal@users.noreply.github.com>
2021-04-08 13:06:07 -0700

    Create a link_list for TinyMCE to query for available site pages

--- packman/pages/urls.py
+++ packman/pages/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path("contact-us/", views.ContactPageView.as_view(), name="contact"),
     path("<slug:slug>/", views.PageDetailView.as_view(), name="detail"),
     path("<slug:slug>/edit", views.PageUpdateView.as_view(), name="update"),
+    path("api/v1/pages/link_list/", views.get_link_list, name="link_list"),
 ]
