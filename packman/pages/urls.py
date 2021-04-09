46d93e817b7e8d5204eb5c34f83b9981f82d8bc9
John Lawrence <thirdjal@users.noreply.github.com>
2021-04-09 10:47:58 -0700

    Allow members with permission to add/edit/delete webpages without needing to go to admin

--- packman/pages/urls.py
+++ packman/pages/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path("signup/", views.SignUpPageView.as_view(), name="signup"),
     path("contact-us/", views.ContactPageView.as_view(), name="contact"),
     path("<slug:slug>/", views.PageDetailView.as_view(), name="detail"),
-    path("<slug:slug>/edit", views.PageUpdateView.as_view(), name="update"),
+    path("page/add/", views.PageCreateView.as_view(), name="create"),
+    path("page/<uuid:pk>/delete/", views.PageDeleteView.as_view(), name="delete"),
+    path("page/<uuid:pk>/edit/", views.PageUpdateView.as_view(), name="update"),
     path("api/v1/pages/link_list/", views.get_link_list, name="link_list"),
 ]
