90c8af7735a53f3cdc546ac7364c7e8689b5c570
John Lawrence <thirdjal@users.noreply.github.com>
2021-07-26 14:36:31 -0700

    Restore ability to add attachments to Message

--- packman/mail/views.py
+++ packman/mail/views.py
@@ -13,6 +13,8 @@ class MessageDetailView(LoginRequiredMixin, DetailView):
     def get_object(self, queryset=None):
         obj = super().get_object(queryset)
         if obj.author != self.request.user and self.request.user not in obj.recipients:
-            raise Http404(_("No %(verbose_name)s found matching the query") % {"verbose_name": queryset.model._meta.verbose_name})
+            raise Http404(
+                _("No %(verbose_name)s found matching the query") % {"verbose_name": queryset.model._meta.verbose_name}
+            )
         obj.mark_read(self.request.user)
         return obj
