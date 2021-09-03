50b149c015fbefe2e9ef2ebcf1e71d6c010a12df
John Lawrence <thirdjal@users.noreply.github.com>
2021-09-03 15:17:03 -0700

    Allow attachments and modifying the DL list in the web interface.

--- packman/mail/views.py
+++ packman/mail/views.py
@@ -1,12 +1,13 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.http import Http404, HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
-from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
 
-from .forms import MessageDistributionFormSet, MessageForm
-from .models import Mailbox, Message
+from .forms import AttachmentForm, MessageDistributionFormSet, MessageForm
+from .models import Mailbox, Message, Attachment
 from .utils import get_mailbox_counts
 
 
@@ -23,19 +24,28 @@ class MessageCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
         context["mail_count"] = get_mailbox_counts(self.request.user)
 
         if self.request.POST:
+            context["attachments_form"] = AttachmentForm(self.request.POST, self.request.FILES)
             context["dl_formset"] = MessageDistributionFormSet(self.request.POST, prefix="to_field")
         else:
+            context["attachments_form"] = AttachmentForm()
             context["dl_formset"] = MessageDistributionFormSet(prefix="to_field")
         return context
 
     def form_valid(self, form):
         context = self.get_context_data(form=form)
         dl_formset = context["dl_formset"]
+        attachments_form = context["attachments_form"]
+        files = self.request.FILES.getlist("attachments")
 
-        if dl_formset.is_valid():
+        if attachments_form.is_valid() and dl_formset.is_valid():
             form.instance.author = self.request.user
             obj = form.save()
 
+            # Save the files
+            for file in files:
+                Attachment.objects.create(message=obj, filename=file)
+
+            # Save the distribution lists
             dl_formset.instance = obj
             dl_formset.save()
 
@@ -67,21 +77,30 @@ class MessageUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
         context["mail_count"] = get_mailbox_counts(self.request.user, context["mailbox"])
         context["message_list"] = Message.objects.drafts(self.request.user)
         if self.request.POST:
+            context["attachments_form"] = AttachmentForm(self.request.POST, self.request.FILES)
             context["dl_formset"] = MessageDistributionFormSet(
                 self.request.POST, prefix="to_field", instance=self.object
             )
         else:
+            context["attachments_form"] = AttachmentForm(self.request.POST, self.request.FILES)
             context["dl_formset"] = MessageDistributionFormSet(prefix="to_field", instance=self.object)
         return context
 
     def form_valid(self, form):
         context = self.get_context_data(form=form)
         dl_formset = context["dl_formset"]
+        attachments_form = context["attachments_form"]
+        files = self.request.FILES.getlist("attachments")
 
-        if dl_formset.is_valid():
+        if attachments_form.is_valid() and dl_formset.is_valid():
             form.instance.author = self.request.user
             obj = form.save()
 
+            # Save the new files
+            for file in files:
+                Attachment.objects.create(message=obj, filename=file)
+
+            # Save the distribution lists
             dl_formset.instance = obj
             dl_formset.save()
 
@@ -146,6 +165,21 @@ class MessageDetailView(LoginRequiredMixin, DetailView):
             self.object.mark_undeleted(request.user)
 
         return HttpResponseRedirect(self.object.get_absolute_url())
+
+
+class MessageDeleteView(LoginRequiredMixin, DeleteView):
+    model = Message
+    success_url = reverse_lazy("mail:drafts")
+    template_name = "mail/message_confirm_delete.html"
+
+    def delete(self, request, *args, **kwargs):
+        self.object == self.get_object()
+        if self.request.user == self.object.author and not self.object.date_sent:
+            success_message = _("Your message <strong>%(subject)s</strong> was deleted successfully.") % {
+                "subject": self.get_object()
+            }
+            messages.success(request, success_message, "danger")
+            return super().delete(request, *args, **kwargs)
 
 
 class MessageListView(LoginRequiredMixin, ListView):
