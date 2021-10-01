af04fbbcd68ba8a18857e8c60c13263709374021
John Lawrence <thirdjal@users.noreply.github.com>
2021-10-01 14:24:49 -0700

    Simplify URL scheme

--- packman/campaigns/urls.py
+++ packman/campaigns/urls.py
@@ -4,12 +4,12 @@ from .views import OrderCreateView, OrderDeleteView, OrderDetailView, OrderListV
 
 app_name = "campaigns"
 urlpatterns = [
-    path("", ProductListView.as_view(), name="product_list"),
-    path("orders/", OrderListView.as_view(), name="order_list"),
-    path("orders/<int:year>/", OrderListView.as_view(), name="order_list_by_year"),
-    path("orders/new/", OrderCreateView.as_view(), name="order_create"),
-    path("orders/<uuid:pk>/", OrderDetailView.as_view(), name="order_detail"),
-    path("orders/<uuid:pk>/edit/", OrderUpdateView.as_view(), name="order_update"),
-    path("orders/<uuid:pk>/delete/", OrderDeleteView.as_view(), name="order_delete"),
+    path("", OrderListView.as_view(), name="order_list"),
+    path("<int:year>/", OrderListView.as_view(), name="order_list_by_year"),
+    path("new/", OrderCreateView.as_view(), name="order_create"),
+    path("<uuid:pk>/", OrderDetailView.as_view(), name="order_detail"),
+    path("<uuid:pk>/edit/", OrderUpdateView.as_view(), name="order_update"),
+    path("<uuid:pk>/delete/", OrderDeleteView.as_view(), name="order_delete"),
+    path("products/", ProductListView.as_view(), name="product_list"),
     path("api/v1/update_order", update_order, name="api_update"),
 ]
