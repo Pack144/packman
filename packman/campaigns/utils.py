b97a4bce99c49bcddbb3aa239b10344af2d9ada4
John Lawrence <thirdjal@users.noreply.github.com>
2021-10-02 17:32:25 -0700

    Enable emailed receipts

--- packman/campaigns/utils.py
+++ packman/campaigns/utils.py
@@ -1,4 +1,8 @@
-from .models import Order, Family
+from django.contrib.sites.models import Site
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from .models import Order
+
 
 def get_cart_from_user(request, seller):
     order, created = Order.objects.get_or_create(
@@ -12,4 +16,22 @@ def get_cart_from_user(request, seller):
 
 def build_cart(request):
     items, order, seller, cart = get_cart_from_user(request)
-    return {"items": items, "order": order, "seller": seller, "cart": cart }
+    return {"items": items, "order": order, "seller": seller, "cart": cart}
+
+
+def email_receipt(order):
+    site = Site.objects.get_current()
+
+    context = {"site": site, "order": order}
+    subject = render_to_string("campaigns/email/subject.txt", context).strip()
+    plaintext = render_to_string("campaigns/email/message_body.txt", context)
+    richtext = render_to_string("campaigns/email/message_body.html", context)
+
+    msg = EmailMultiAlternatives(
+        subject,
+        plaintext,
+        to=[order.customer.email],
+        alternatives=[(richtext, "text/html")],
+    )
+    print("sending email")
+    msg.send()
