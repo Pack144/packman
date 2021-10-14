297aa83a50c96bf13aa2e6c528af53ee4a6d18d9
John Lawrence <thirdjal@users.noreply.github.com>
2021-10-14 11:37:01 -0700

    Add daily reporting to front-end

--- packman/campaigns/urls.py
+++ packman/campaigns/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from .views import (
     OrderCreateView,
@@ -6,6 +6,7 @@ from .views import (
     OrderDetailView,
     OrderListView,
     OrderUpdateView,
+    OrderReportView,
     ProductListView,
     update_order,
     PrizeListView,
@@ -14,12 +15,14 @@ from .views import (
 app_name = "campaigns"
 urlpatterns = [
     path("", OrderListView.as_view(), name="order_list"),
-    path("<int:campaign>/", OrderListView.as_view(), name="order_list_by_campaign"),
+    path("api/v1/update_order", update_order, name="api_update"),
     path("new/", OrderCreateView.as_view(), name="order_create"),
+    path("products/", ProductListView.as_view(), name="product_list"),
+    path("prizes/", PrizeListView.as_view(), name="prize_list"),
+    path("<int:campaign>/", OrderListView.as_view(), name="order_list_by_campaign"),
     path("<uuid:pk>/", OrderDetailView.as_view(), name="order_detail"),
     path("<uuid:pk>/edit/", OrderUpdateView.as_view(), name="order_update"),
     path("<uuid:pk>/delete/", OrderDeleteView.as_view(), name="order_delete"),
-    path("products/", ProductListView.as_view(), name="product_list"),
-    path("prizes/", PrizeListView.as_view(), name="prize_list"),
-    path("api/v1/update_order", update_order, name="api_update"),
+    path("reports/", OrderReportView.as_view(), name="order_report"),
+    re_path(r"^reports/(?P<campaign>[0-9]{4})/", OrderReportView.as_view(), name="order_report_by_campaign"),
 ]
