6964d3816f7fae0c95d6c365557db711c0d2de7c
John Lawrence <thirdjal@users.noreply.github.com>
2022-04-21 17:10:17 -0700

    Format with Black

--- packman/mail/utils.py
+++ packman/mail/utils.py
@@ -1,7 +1,8 @@
+import logging
+
 from django.contrib.sites.models import Site
 from django.core.mail import EmailMultiAlternatives
 from django.urls import reverse
-import logging
 
 logger = logging.getLogger(__name__)
 
@@ -11,6 +12,7 @@ class ListEmailMessage(EmailMultiAlternatives):
     A version of EmailMultiAlternatives customized for sending messages to
     an email list.
     """
+
     def __init__(
         self,
         subject="",
@@ -33,7 +35,7 @@ class ListEmailMessage(EmailMultiAlternatives):
             from_email = from_email or self.settings.from_email
             subject = f"{self.settings.subject_prefix} {subject}".strip()
             headers = headers or {}
-            headers['List-Id'] = f"<{self.settings.list_id}> {self.settings.name}".strip()
+            headers["List-Id"] = f"<{self.settings.list_id}> {self.settings.name}".strip()
             headers["List-Unsubscribe"] = f"<https://{site.domain}{reverse('membership:my-family')}>"
 
         super().__init__(
