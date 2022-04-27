6d2253a921d00b28f636df12ee95e5b26a5bad67
John Lawrence <thirdjal@users.noreply.github.com>
2022-04-27 15:20:01 -0700

    Unused code cleanup

--- packman/mail/management/commands/sendemails.py
+++ packman/mail/management/commands/sendemails.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.utils.translation import gettext as _
 
 from packman.mail.models import Message
