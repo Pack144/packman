c36514027a676455c0d3b8ecd2109d2e363eb422
John Lawrence <thirdjal@users.noreply.github.com>
2022-05-05 09:29:06 -0700

    Leverage uWSGI timers to send emails when available.

--- packman/wsgi.py
+++ packman/wsgi.py
@@ -7,10 +7,26 @@ For more information on this file, see
 https://docs.djangoproject.com/en/stable/howto/deployment/wsgi/
 """
 
+import logging
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.utils.translation import gettext_lazy as _
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "packman.settings.production")
 
 application = get_wsgi_application()
+logger = logging.getLogger(__name__)
+
+try:
+    from django.core.management import call_command
+
+    import uwsgidecorators
+
+    @uwsgidecorators.timer(10)
+    def send_emails(num):
+        """Send queued emails every 10 seconds"""
+        call_command("sendemails")
+
+except ImportError:
+    logger.info(_("Module uwsgidecorators not found. Timers are disabled."))
