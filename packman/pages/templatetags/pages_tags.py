8d9850ec7dff28a23d8d196bac8af48f84c0ea69
John Lawrence <thirdjal@noreply.users.github.com>
2022-08-23 15:38:54 -0700

    refactor paginator to collapse on large number of pages

--- packman/pages/templatetags/pages_tags.py
+++ packman/pages/templatetags/pages_tags.py
@@ -3,6 +3,24 @@ from django import template
 register = template.Library()
 
 
+@register.simple_tag
+def get_elided_page_range(page_obj, on_each_side=3, on_ends=2):
+    """
+    Return a 1-based range of pages with some values elided.
+
+    If the page range is larger than a given size, the whole range is not
+    provided and a compact form is returned instead, e.g. for a paginator
+    with 50 pages, if page 43 were the current page, the output, with the
+    default arguments, would be:
+
+        1, 2, …, 40, 41, 42, 43, 44, 45, 46, …, 49, 50.
+
+    https://docs.djangoproject.com/en/3.2/ref/paginator/#django.core.paginator.Paginator.get_elided_page_range
+    """
+    paginator = page_obj.paginator
+    return paginator.get_elided_page_range(number=page_obj.number, on_each_side=on_each_side, on_ends=on_ends)
+
+
 @register.simple_tag(takes_context=True)
 def query_transform(context, **kwargs):
     """
