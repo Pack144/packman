2597740e257d71db670283cd5a56e76f4f3d6c21
John Lawrence <thirdjal@noreply.users.github.com>
2023-03-01 15:57:37 -0800

    Enable security screening with Bandit

--- packman/membership/tests.py
+++ packman/membership/tests.py
@@ -6,7 +6,7 @@ User = get_user_model()
 
 class AdultManagersTests(TestCase):
     def test_create_user(self):
-        user = User.objects.create_user(email="normal@example.com", password="foo")
+        user = User.objects.create_user(email="normal@example.com", password="foo")  # nosec B106
         self.assertEqual(user.email, "normal@example.com")
         self.assertTrue(user.is_active)
         self.assertFalse(user._is_staff)
@@ -16,13 +16,13 @@ class AdultManagersTests(TestCase):
         with self.assertRaises(ValueError):
             User.objects.create_user(email="")
         with self.assertRaises(ValueError):
-            User.objects.create_user(email="", password="foo")
+            User.objects.create_user(email="", password="foo")  # nosec B106
 
     def test_create_superuser(self):
-        admin_user = User.objects.create_superuser("super@example.com", "foo")
+        admin_user = User.objects.create_superuser("super@example.com", "foo")  # nosec B106
         self.assertEqual(admin_user.email, "super@example.com")
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user._is_staff)
         self.assertTrue(admin_user.is_superuser)
         with self.assertRaises(ValueError):
-            User.objects.create_superuser(email="super@user.com", password="foo", is_superuser=False)
+            User.objects.create_superuser(email="super@user.com", password="foo", is_superuser=False)  # nosec B106
