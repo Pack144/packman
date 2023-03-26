a3dc79b163ac720148d810bd99787948d25cd481
John Lawrence <thirdjal@noreply.users.github.com>
2023-03-25 22:59:56 -0700

    Update development settings

--- packman/settings/local.py
+++ packman/settings/local.py
@@ -1,6 +1,8 @@
+import contextlib
+
 from .base import *
 
-# Quick-start development settings - unsuitable for production
+# Development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/stable/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -8,25 +10,59 @@ SECRET_KEY = env(
     "DJANGO_SECRET_KEY",
     default="django-insecure-c#p2tsup@!p3yjxxoi9dbk)xb_iq1e5*_x&n)d$!*hc8@wuh*f",
 )
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG", default=True)
-
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=[])
+
+
+# Logging
+# https://docs.djangoproject.com/en/stable/topics/logging/
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": env("DJANGO_LOG_LEVEL", default="INFO"),
+            "propagate": False,
+        },
+    },
+}
+
+
+# Crispy Forms
+# https://django-crispy-forms.readthedocs.io/en/latest/crispy_tag_forms.html#make-crispy-forms-fail-loud
+
+CRISPY_FAIL_SILENTLY = not DEBUG
+
 
 # Django Debug Toolbar
 # https://django-debug-toolbar.readthedocs.io/en/latest/
-INSTALLED_APPS += ["debug_toolbar"]
-MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
-INTERNAL_IPS = env.list("INTERNAL_IPS", default=["127.0.0.1"])
+
+with contextlib.suppress(ImportError):
+    INSTALLED_APPS += ["debug_toolbar"]
+    INTERNAL_IPS = env.list("INTERNAL_IPS", default=["127.0.0.1"])
+    MIDDLEWARE.insert(
+        MIDDLEWARE.index("django.middleware.common.CommonMiddleware") + 1,
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
+    )
+
 
 # Django Extensions
 # https://django-extensions.readthedocs.io/en/latest/
+
 INSTALLED_APPS += ["django_extensions"]
+
 
 # Whitenoise
 # http://whitenoise.evans.io/en/latest/django.html#using-whitenoise-in-development
 INSTALLED_APPS += ["whitenoise.runserver_nostatic"]
-
-# Crispy Forms
-CRISPY_FAIL_SILENTLY = not DEBUG
