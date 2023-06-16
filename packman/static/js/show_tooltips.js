7a701928dbb76190a67ed4b2110d70e9de94ca0d
John Lawrence <thirdjal@noreply.users.github.com>
2023-06-16 10:52:31 -0700

    refactor tooltips javascript

--- packman/static/js/show_tooltips.js
+++ packman/static/js/show_tooltips.js
@@ -1,3 +1,4 @@
+/* Enable tooltips */
 let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
