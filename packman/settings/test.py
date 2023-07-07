38415adb179f64612a014a85905d42bfa3e6c4e4
John Lawrence <thirdjal@noreply.users.github.com>
2023-07-07 08:57:05 -0700

    Update settings for 4.2

--- packman/settings/test.py
+++ packman/settings/test.py
@@ -1,5 +1,7 @@
 from .base import *
 
+print(f"Using settings '{__name__}'")
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("DJANGO_SECRET_KEY", default="django-insecure")
 
@@ -8,9 +10,16 @@ DEBUG = env("DJANGO_DEBUG", default=False)
 
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=[])
 
-# https://docs.djangoproject.com/en/stable/ref/settings/#password-hashers
+# https://docs.djangoproject.com/en/4.2/ref/settings/#password-hashers
 PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
-# https://docs.djangoproject.com/en/stable/ref/contrib/staticfiles/#django.contrib.staticfiles.storage.ManifestStaticFilesStorage.manifest_strict
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+# https://docs.djangoproject.com/en/4.2/ref/contrib/staticfiles/
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.InMemoryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
 # http://whitenoise.evans.io/en/latest/django.html#whitenoise-makes-my-tests-run-slow
 WHITENOISE_AUTOREFRESH = True
