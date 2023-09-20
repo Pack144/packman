092d009f7915a7b72624b3990ece13717ffec860
texnofobix <texnofobix@gmail.com>
2023-09-20 07:42:20 -0400

    add ability to track committee meeting attendance

--- packman/attendance/models.py
+++ packman/attendance/models.py
@@ -4,10 +4,21 @@ from django.utils.translation import gettext_lazy as _
 from packman.core.models import TimeStampedUUIDModel
 from packman.calendars.models import Event
 from packman.membership.models import Member
+from packman.committees.models import Committee
 
 class Attendance(TimeStampedUUIDModel):
-    event = models.OneToOneField(Event, on_delete=models.CASCADE)
+    event = models.OneToOneField(
+        Event,
+        on_delete=models.CASCADE,
+    )
     member = models.ManyToManyField(Member)
+    committee = models.ForeignKey(
+        Committee,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+
 
     class Meta:
         # indexes = [models.Index(fields=["name"])]
