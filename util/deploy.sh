493777b3fa16fbb7d3b4dadf2a15e916d0e72634
Joshua Royalty <royalpeasantry@gmail.com>
2026-08-04 12:26:45 -0700

    Add server restart

--- util/deploy.sh
+++ util/deploy.sh
@@ -34,14 +34,14 @@ if [[ "$CONFIRM" != "y" && "$CONFIRM" != "yes" ]]; then
     exit 3
 fi
 
-# Update dependencies if uv.lock changed
-echo "Updating dependencies with uv sync"
+echo "Updating dependencies"
 UV_PROJECT_ENVIRONMENT="$APP_DIR/env" uv sync --group production
 
-# Do any database migrations that have not been applied
 echo "Running database migrations"
 $APP_DIR/env/bin/python manage.py migrate
 
-# Collect any new static files
 echo "Collecting any new static files"
 DJANGO_SETTINGS_MODULE=packman.settings.production $APP_DIR/env/bin/python manage.py collectstatic --no-input
+
+echo "Initiating server restart"
+touch $APP_DIR/packman/packman/wsgi.py
